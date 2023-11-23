ac38c53ed167f699331d658da6e97aa083a98f4f
Camille Boujasson <camilleb.dev@outlook.fr>
2023-11-23 15:16:11 +0100

    fetch n state

--- src/pages/CocktailsPage.jsx
+++ src/pages/CocktailsPage.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react"
+
 function CocktailsPage () {
+
+    const [drinks, setDrinks] = useState()
+
+    if(!drinks) {
 
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=") // via fetch on envoie une requête à l'API 
     .then((response) => { // on utilise .then pour gérer la réponse de l'API 
         return response.json() // on convertit la data retournée en json
     })
     .then((json) => { // .then gère la data en json et la rend accessible
-        return console.log(json) // on affiche en console la data récupérée
+        setDrinks(json) // on affiche en console la data récupérée
     })
+    } 
 
-    return <p>hi</p>
+    return <div>
+        {drinks ? 
+        <p>Drinks r ready</p>
+        : 
+        <p>Loading</p>
+        }
+    </div>
 }
 
 export default CocktailsPage; 
