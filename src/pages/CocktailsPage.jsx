6203c30b8a31688164b80274bdb5e5b1f4244ff9
Camille Boujasson <camilleb.dev@outlook.fr>
2023-11-23 16:59:16 +0100

    display fetch data

--- src/pages/CocktailsPage.jsx
+++ src/pages/CocktailsPage.jsx
@@ -2,26 +2,34 @@ import { useState } from "react"
 
 function CocktailsPage () {
 
-    const [drinks, setDrinks] = useState()
+    const [ddrinks, setDdrinks] = useState()
 
-    if(!drinks) {
+    if(!ddrinks) { // le fetch est lancé que s'il n'a pas déjà été chargé (que la valeur de drinks est vide)
 
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=") // via fetch on envoie une requête à l'API 
     .then((response) => { // on utilise .then pour gérer la réponse de l'API 
         return response.json() // on convertit la data retournée en json
     })
     .then((json) => { // .then gère la data en json et la rend accessible
-        setDrinks(json) // on affiche en console la data récupérée
+        setDdrinks(json.drinks) // on affiche en console la data récupérée dans le useState 
     })
     } 
 
-    return <div>
-        {drinks ? 
-        <p>Drinks r ready</p>
-        : 
-        <p>Loading</p>
-        }
-    </div>
+    return (
+        <main>
+            {ddrinks ? ( // si drinks a été récupéré
+                <>
+                {ddrinks.map((drinks) => { // je boucle sur la data avec map 
+                    return (
+                        <h3>{drinks.strDrink}</h3> // je demande de récupérer et rendre la propriété .strDrink du tableau
+                    )
+                })}
+                </>
+            ) : (
+                <p>Loading</p> // si drinks n'a pas été récupéré j'affiche ce message 
+            )}
+        </main>
+    )
 }
 
 export default CocktailsPage; 
